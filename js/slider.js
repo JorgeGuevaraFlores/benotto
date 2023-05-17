7d1409ba8f015a164cea4a713a630d79f018691f
Jorge Guevara Flores <jorgegf720@gmail.com>
2023-05-17 16:24:40 -0600

    Página inicio hecho, responsiva

---
+++ js/slider.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+
+  let currentSlide = 0;
+
+  function showSlide() {
+    slides[currentSlide].classList.add("active");
+  }
+
+  function hideSlides() {
+    slides.forEach((slide) => slide.classList.remove("active"));
+  }
+
+  function nextSlide() {
+    hideSlides();
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide();
+  }
+
+  function prevSlide() {
+    hideSlides();
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide();
+  }
+
+  // Cambiar al siguiente slide al hacer clic en la flecha de siguiente
+  nextButton.addEventListener("click", nextSlide);
+
+  // Cambiar al slide anterior al hacer clic en la flecha de anterior
+  prevButton.addEventListener("click", prevSlide);
+
+  showSlide();
+});
